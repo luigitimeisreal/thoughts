ee8caf5963c60ec5b8b1ebf2c310396f8a87f8a7
Luigitime! <antoniors2003@gmail.com>
2024-08-16 14:18:26 +0200

    Changed routes

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,22 @@
+const colors = require('tailwindcss/colors')
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
