2dfaea187194bb45f4d3b9d0cac2803638f7d852
luigitimeisreal <antoniors2003@gmail.com>
2024-10-10 12:39:13 +0200

    Added UI for home page

---
+++ frontend/src/app/components/new/new.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'new',
+  standalone: true,
+  imports: [],
+  templateUrl: './new.component.html',
+  styleUrl: './new.component.css'
+})
+export class NewComponent {
+
+}
--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { TextPostComponent } from '../../components/text-post/text-post.component';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { NewComponent } from '../../components/new/new.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TextPostComponent, NavbarComponent],
+  imports: [TextPostComponent, NavbarComponent, NewComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
